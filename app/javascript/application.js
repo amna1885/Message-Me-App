7da123e94652e5aac71ce166cece023d0fd3894a
amna <amna.mansoor@devsinc.com>
2024-07-17 22:32:11 +0500

    Messages Functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,5 +6,14 @@ import "semantic-ui-sass"; // Import Semantic UI or other libraries
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
   // Other jQuery initialization as needed
-});
+})
+
