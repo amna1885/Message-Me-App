e309c984523de47aee75d1cc5bb71bc0ee9518bc
amna <amna.mansoor@devsinc.com>
2024-07-17 17:25:11 +0500

    Initial Setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// app/javascript/packs/application.js
+
+import $ from 'jquery'; // Import jQuery
+import "semantic-ui-sass"; // Import Semantic UI or other libraries
+
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  // Other jQuery initialization as needed
+});
